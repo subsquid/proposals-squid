1735b5993f2b59c4c4df7fa6a457227f4566cf58
Nikita Klimin <nklimin007@gmail.com>
2022-07-12 22:24:46 +0300

    fix: remove old parsers

--- src/mappings/bounties/events/bountyAwarded.ts
+++ src/mappings/bounties/events/bountyAwarded.ts
@@ -2,11 +2,10 @@ import { EventHandlerContext } from '../../types/contexts'
 import { ProposalStatus, ProposalType } from '../../../model'
 import { ss58codec } from '../../../common/tools'
 import { updateProposalStatus } from '../../utils/proposals'
-import { getBountyAwardedData, getBountyAwardedDataOld } from './getters'
+import { getBountyAwardedData } from './getters'
 
 export async function handleAwarded(ctx: EventHandlerContext) {
-    const section = ctx.event.name.split('.')[0]
-    const getEventData = section === 'Bounties' ? getBountyAwardedData : getBountyAwardedDataOld
+    const getEventData = getBountyAwardedData
     const { index, beneficiary } = getEventData(ctx)
 
     await updateProposalStatus(ctx, index, ProposalType.Bounty, {
